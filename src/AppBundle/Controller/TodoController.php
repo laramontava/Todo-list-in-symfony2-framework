31ea05e19c4c398f4af85f16ba00a4af1d141c99
laramontava <laramontava@gmail.com>
2017-02-22 20:16:11 +0100

    You can edit items. Working with sign up and log in

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -106,14 +106,71 @@ class TodoController extends Controller
          
          $sn->remove($todo);
          $sn->flush();
-        //  $todos = $this->getDoctrine()
-        // ->getRepository('AppBundle:Todo')
-        // ->find($id);
         
          $this->addFlash(
                 'notice',
                 'Todo Removed'
             );
           return $this->redirectToRoute('todo_list');             
+    }
+
+    /**
+     * @Route("/todos/edit/{id}", name="todo_edit")
+     */
+    public function editAction($id,Request $request)
+    {
+         $now = new\DateTime('now');  
+         $todo = $this->getDoctrine()
+        ->getRepository('AppBundle:Todo')
+        ->find($id);
+        
+            $todo->setName($todo->getName());
+            $todo->setCategory($todo->getCategory());          
+            $todo->setDescription($todo->getDescription());                  
+            $todo->setDuedate($todo->getDueDate());          
+            $todo->setCreateDate($now);        
+      
+        $form = $this->createFormBuilder($todo)
+        ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('category', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        
+        ->add('due_date', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+        ->add('Save', SubmitType::class, array('label'=> 'Update Todo', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
+        ->getForm();
+        
+        $form->handleRequest($request);
+        if($form->isSubmitted() &&  $form->isValid()){
+            $name = $form['name']->getData();
+            $category = $form['category']->getData();
+            $description = $form['description']->getData();
+            $due_date = $form['due_date']->getData();
+            $name = $form['name']->getData();
+            
+           
+            $sn = $this->getDoctrine()->getManager();
+            $todo = $sn->getRepository('AppBundle:Todo')->find($id);
+            
+            $todo->setName($name);
+            $todo->setCategory($category);          
+            $todo->setDescription($description);                  
+            $todo->setDuedate($due_date);          
+            $todo->setCreateDate($now);          
+      
+            $sn -> flush();
+            
+            $this->addFlash(
+                'notice',
+                'Todo Updated'
+            );
+            return $this->redirectToRoute('todo_list');            
+           
+        }
+        
+        return $this->render('todos/edit.html.twig', array(
+            'todo' => $todo,
+            'form' => $form->createView()
+        ));
+        
     }
 }
